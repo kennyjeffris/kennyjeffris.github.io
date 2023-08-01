ba9f7a43a9a1758948fdd22ec110bbb5e410e05f
kennyjeffris <kennyjeffris@gmail.com>
2023-07-31 20:17:21 -0700

    added actions, statiq

---
+++ kennyjeffris.github.io/Program.cs
@@ -0,0 +1,8 @@
+﻿return await Bootstrapper
+  .Factory
+  .CreateWeb(args)
+  .DeployToGitHubPages(
+    "kennyjeffris",
+    "kennyjeffris.github.io",
+    Config.FromSetting<string>("GITHUB_TOKEN"))
+  .RunAsync();
